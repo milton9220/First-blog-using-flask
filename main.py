aa391e811785e94632dd7b68f31641e0d857ff5f
Milton <monjurhasan105142@gmail.com>
2020-05-21 16:24:51 +0600

    added post method in dashboard function

--- main.py
+++ main.py
@@ -53,9 +53,17 @@ def home():
 def about():
     return render_template('about.html',pass_param=params)
 
-@app.route('/dashboard')
+@app.route('/dashboard',methods=['GET','POST'])
 def dashboard():
-    return render_template('login.html',pass_param=params)
+    if request.method=='POST':
+        pass
+    else:
+        return render_template('login.html',pass_param=params)    
+	
+	
+
+    
+    	 
 
 @app.route('/contact',methods=['GET','POST'])
 def contact():
